eeee210922c91d1f4e9b84ecfa17696eb7bc3339
Jun Shim <jjunsh.120@gmail.com>
2025-04-12 00:24:39 +0900

    Modified fuzz

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import flask
 import flask_socketio
 
 from datetime import datetime, timezone
-from fuzzywuzzy import fuzz
+from thefuzz import fuzz
 
 from source.gemini import Gemini
 from source.chatgpt import ChatGPT

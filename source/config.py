e7de4e95953b2a9c7df02d78d48ae1fc115881a2
Jun Shim <jjunsh.120@gmail.com>
2025-08-05 15:40:30 +0900

    Modified open with utf-8

--- source/config.py
+++ source/config.py
@@ -4,4 +4,5 @@ import configparser
 config_path = os.path.join(os.path.dirname(__file__), "../config.ini")
 
 CONFIG = configparser.ConfigParser()
-CONFIG.read(config_path)
+with open(config_path, 'r', encoding='utf-8') as f:
+    CONFIG.read_file(f)
